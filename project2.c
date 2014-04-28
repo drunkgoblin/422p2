f054b89d01fcd44b625793bc7c28271ca53e713a
Drunkgoblin <glc4@uw.edu>
2014-04-28 15:33:22 -0700

    second commit

---
+++ project2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <pthread.h>
+
+struct {
+    int id;
+    int phases;
+    int current_phase;
+    // Phase types: 1 = CPU phase; 2 = IO phase
+    int phase_and_dur[phases][phases];
+    int completed;
+};
+
+
+int main() {
+
+}
