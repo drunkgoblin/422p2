05f4ac37fd7ec023009bdf6caf67b3d8fd6ff9ef
kirbok <korbok@hotmail.com>
2014-04-29 18:10:48 -0700

    Makin threadz

--- project2.c
+++ project2.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
 
-struct stuff {
+struct job {
     int id;
     int phases;
     int current_phase;
@@ -11,9 +12,9 @@ struct stuff {
 };
 
 struct queue {
-    struct stuff data;
+    struct job data;
     struct queue * next;
-}head;
+};
 
 struct queue * readrunning;
 struct queue * waitingio;
@@ -36,18 +37,35 @@ int pop() {
 }
 
 int main() {
+    pthread_t threadz[16];
+    int i;
+    for(i = 0; i <= 7; i ++)
+    {
+	pthread_create(&threadz[i],NULL,int cpu,(*)(void *));
+    }
+    for(i = 8; i <= 11; i ++)
+    {
+	pthread_create(&threadz[i],NULL,int iostuff,(*)(void *));
+    }
+    for(i = 12; i <= 15; i ++)
+    {
+	pthread_create(&threadz[i],NULL,int job, (*)(void *));
+    }
+/*
     struct queue piggy;
     readrunning = &piggy;
     readrunning->data.id = 3;
     struct queue phillipe;
     readrunning->next = &phillipe;
     readrunning->next->data.id = 17;
-    struct queue shit;
+    struct queue shit;  
     waitingio = &shit;
+    finished = malloc(sizeof(struct job));
     waitingio->data.id = 4;
-    //finished->data->id = 5;
+    finished->data.id = 5;
     printf("%d\n",readrunning->data.id);
     printf("%d\n",readrunning->next->data.id);
     printf("%d\n",waitingio->data.id);
-    //printf("%d\n",finished->data->id);
+    printf("%d\n",finished->data.id);
+*/
 }
