cc70ed82d563a7f2ea9064b7db2823558c3cc193
melaku <melepyaar@yahoo.com>
2014-05-04 09:25:18 -0700

    Update project2.c

--- project2.c
+++ project2.c
@@ -14,6 +14,7 @@ struct job {
 };
 
 struct queue {
+    int size;
     struct job data;
     struct queue * next;
 };
@@ -23,12 +24,13 @@ struct queue * waitingio;
 struct queue * finished;
 
 //A newly created job
-struct job *newly_created_job;
+struct job *newly_created_job = NULL;
 
 void *job(int job_id) {
 printf("I've got a job for you whore\n");
-struct job current_job;
-newly_created_job = &current_job;
+
+newly_created_job = (struct job *) malloc(sizeof(struct job));
+
 newly_created_job->id = job_id;
 //Number of phases is 2 (cpu and IO)
 newly_created_job->phases = 2;
@@ -36,11 +38,13 @@ newly_created_job->phases = 2;
 newly_created_job->current_phase = 1;
 newly_created_job->completed = 0; //not completed yet
 newly_created_job-> phase_and_dur[0][0] = 1;
+
 printf("The newly created job id is %d\n", newly_created_job->id);
 
 }
 
 void *cpu() {
+
 printf("cpu Thread yo\n");
 //update the job data structure with the completed phase
 int i ;
@@ -59,7 +63,8 @@ job_iterator = waitingio;
 // readrunning ->next = wa
 }
 */
-printf("Job %d has been added to the running queue.\n", newly_created_job->id);
+
+//printf("Job %d has been added to the running queue.\n", newly_created_job->id);
 }
 
 //add jobs to the I/O queue if CPU phase is completed and next phase is I/O bound
@@ -71,12 +76,67 @@ printf("Job %d has been added to the running queue.\n", newly_created_job->id);
 void *iostuff() {
 printf("io thread bitch\n");
 }
+/**
+ * Remove the first item in the queue
+**/
+struct job *pop(struct queue *the_queue) {
 
-int pop() {
+struct job *next_job = &the_queue->data;
+struct queue *current = the_queue;
+while(current != NULL)
+{
+next_job = &current->data;
+
+current = current->next;
+
 
 }
+return next_job;
+}
+/**
+*Add item to the queue
+* queue type 1 = readyrunning, 2 = waiting, 3 = finished
+**/
+struct queue *add(int queue_type, struct job *the_job){
+struct queue *the_queue;
+
+switch(queue_type)
+{
+case 1: 
+    the_queue = readrunning;
+    break;
+case 2:
+    the_queue = waitingio;
+    break;
+case 3:
+    the_queue = finished;
+    break;
+}
+if(the_queue->size == 0)//queue is empty
+{
+  the_queue->data = *the_job;
+  the_queue->next = NULL;
+  
+}
+else//queue is not empty, so add a job infront of it. FIFO
+{
+   struct queue *new_queue = 
+             (struct queue *) malloc(sizeof(struct queue));
+   new_queue->data = *the_job;
+   new_queue->next = the_queue;   
+}
+the_queue->size++;
+
+return the_queue;
+
+} 
 
 int main() {
+/**
+//initialize 
+newly_created_job = (struct job *) malloc(sizeof(struct job));
+
+
     pthread_t threadz[16];
     int i;
     for(i = 0; i <= 7; i++)
@@ -89,15 +149,16 @@ pthread_create(&threadz[i],NULL,iostuff,NULL);
     }
     for(i = 12; i <= 15; i++)
     {
-pthread_create(&threadz[i],NULL,job(3), NULL);
+//job number starts at 0
+pthread_create(&threadz[i],NULL,job(i-12), NULL);
     }
     printf("yar\n");
     for(i=0; i < 15; i++)
 {
 pthread_join(threadz[i],NULL);
 }
+pthread_exit(NULL);
 
-/*
 struct queue piggy;
 readrunning = &piggy;
 readrunning->data.id = 3;
@@ -114,5 +175,11 @@ printf("%d\n",readrunning->next->data.id);
 printf("%d\n",waitingio->data.id);
 printf("%d\n",finished->data.id);
 */
+
+job(1); //create a job
+readrunning = (struct queue *) malloc(sizeof(struct queue));
+readrunning = add(3, newly_created_job);
+printf("First test %d\n", readrunning->data.id);
+printf("Removed job is =  %d\n", pop(readrunning)->id);
 }
 
